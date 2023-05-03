5de2f51560c354a54108da7d59f8c74e334fc1e3
Arnon Paiva <arnongp@gmail.com>
2023-05-03 20:29:10 -0300

    projeto final

---
+++ main.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form-agenda')
+
+const nomes = []
+const telefones = []
+
+let linhas = ''
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+
+  adicionaLinha()
+  atualizaTabela()
+})
+
+function adicionaLinha() {
+  const inputNomeContato = document.getElementById('nome-contato')
+  const inputNumeroContato = document.getElementById('numero-contato')
+
+  if (nomes.includes(inputNomeContato.value)) {
+    alert(`O contato ${inputNomeContato.value} já foi inserido.`)
+  } else {
+  }
+
+  nomes.push(inputNomeContato.value)
+  telefones.push(inputNumeroContato.value)
+
+  let linha = '<tr>'
+  linha += `<td>${inputNomeContato.value}</td>`
+  linha += `<td>${inputNumeroContato.value}</td>`
+  linha += '</tr>'
+
+  linhas += linha
+
+  inputNomeContato.value = ''
+  inputNumeroContato.value = ''
+}
+
+function atualizaTabela() {
+  const corpoTabela = document.querySelector('tbody')
+  corpoTabela.innerHTML = linhas
+}
